a188542a3f3484f93a8272819b6dc56ec7f30557
Jannik Kuhlmann <mail@jannikkuhlmann.de>
2026-05-27 20:34:43 +0200

    venv fix

--- src/pipelines/pipelineA/A-01-embed-test.sh
+++ src/pipelines/pipelineA/A-01-embed-test.sh
@@ -22,7 +22,11 @@ ml GCCcore/13.3.0
 ml Python/3.12.3
 
 # SOURCE PYTHON VENV
-source $HOME/venvs/colpali-20260517/bin/activate
+
+python -m venv $HOME/venvs/colpali-a100
+source $HOME/venvs/colpali-a100/bin/activate
+pip install torch colpali-engine pymupdf python-dotenv pillow
+# source $HOME/venvs/colpali-20260517/bin/activate
 
 # SET EXISTING CHACHE
 export HF_HOME=$WORK/cache/huggingface 
