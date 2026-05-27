9ac5a910747c4667735e9a71525bb0ed8e094aca
Jannik Kuhlmann <mail@jannikkuhlmann.de>
2026-05-27 20:06:20 +0200

    More Comments.

--- src/pipelines/pipelineA/A-01-embed.py
+++ src/pipelines/pipelineA/A-01-embed.py
@@ -1,5 +1,6 @@
 import time
 import os
+import argparse
 import torch
 import fitz  # pymupdf
 
@@ -9,7 +10,12 @@ from PIL import Image
 from colpali_engine.models import ColPali, ColPaliProcessor
 
 # Loading API-Keys and Tokens via local .env
-load_dotenv(find_dotenv)
+load_dotenv(find_dotenv())
+
+# ── Arguments for Dev'ing
+parser = argparse.ArgumentParser()
+parser.add_argument("-t", action="store_true", help="Toggle Testing Path")
+args = parser.parse_args()
 
 #### Helping Functions ##########################################
 
@@ -26,7 +32,7 @@ def banner(title):
 MODEL_NAME = "vidore/colpali-v1.3"
 
 # Path To All and List Of All Paths to ESG-Reports
-PDF_DIR  = Path("$WORK/data/esg_reports")
+PDF_DIR  = Path("/scratch/tmp/jkuhlma1/data/esg_reports_tests") if args.t else Path("/scratch/tmp/jkuhlma1/data/esg_reports")
 PDF_LIST = list(PDF_DIR.glob("*.pdf"))
 
 BATCH_SIZE = 8 #8 seems to work well on H200mini nodes
@@ -36,7 +42,7 @@ SAVE_DIR = Path("/scratch/tmp/jkuhlma1/data/embeddings")
 
 # May ommit ...
 #### 1. GPU Details #############################################
-banner("SCHRITT 1: GPU / CUDA")
+banner("STEP 1: GPU / CUDA")
 gpu_name   = torch.cuda.get_device_name(0)
 vram_total = torch.cuda.get_device_properties(0).total_memory / 1e9
 print(f"  GPU  : {gpu_name}")
@@ -45,6 +51,7 @@ print(f"  VRAM : {vram_total:.1f} GB")
 
 
 #### 2. Load ColPali ############################################
+banner("STEP 2: Load ColPali")
 model = ColPali.from_pretrained(
     MODEL_NAME,
     torch_dtype=torch.bfloat16,
@@ -57,6 +64,7 @@ print(f"  VRAM belegt: {torch.cuda.max_memory_allocated() / 1e9:.1f} GB")
 
 
 #### 3. PDF to Image ############################################
+banner("STEP 3: PDF to Image")
 # => Das würde jetzt erstmal alle PDFs in Bilder umwandeln ...
 # Man könnte das natürlich auslagern und nur genau das PDF umwandeln, was man grad braucht ...
 
@@ -80,7 +88,9 @@ for pdf_path in PDF_LIST :
 
 
 #### 4. Create Embeddings #######################################
+banner("STEP 4: Create Embeddings")
 
+# Iterating trough all PDFs in pdf_img_dict
 for report_name, report_imgs in pdf_img_dict.items():
     
     ## This intermediate step is necessary to not overlad VRAM with all pages at once
@@ -106,4 +116,7 @@ for report_name, report_imgs in pdf_img_dict.items():
     report_tensor = torch.cat(report_embeddings, dim=0)
     torch.save(report_tensor.cpu(), f"{SAVE_DIR}/{report_name}.pt") #.cpu() redundant, but clear that everything is unbounded from GPU
     
-    
+    print(f"Tensor for {report_name} saved.")
+
+print(f"All Tensors saved to {SAVE_DIR}")
+banner("DONE.")
