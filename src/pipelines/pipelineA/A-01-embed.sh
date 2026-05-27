55ffbe543b4cd25d181a5df5af831e62de169aef
Jannik Kuhlmann <mail@jannikkuhlmann.de>
2026-05-27 21:24:29 +0200

    All .sh to A100

--- src/pipelines/pipelineA/A-01-embed.sh
+++ src/pipelines/pipelineA/A-01-embed.sh
@@ -4,9 +4,9 @@
 #SBATCH --ntasks-per-node=1         # the number of tasks/processes per node
 
 #SBATCH --gres=gpu:1
-#SBATCH --mem=8G
-#SBATCH --cpus-per-task=4           # the number cpus per task
-#SBATCH --partition=gpuh200mini     # on which partition to submit the job
+#SBATCH --mem=50G
+#SBATCH --cpus-per-task=8           # the number cpus per task
+#SBATCH --partition=gpua100     # on which partition to submit the job
 #SBATCH --time=00:40:00             # the max wallclock time (time limit your job will run)
 
 #SBATCH --output=/scratch/tmp/jkuhlma1/logs/%j_out.log    # stdout → Datei (%j = Job-ID)
