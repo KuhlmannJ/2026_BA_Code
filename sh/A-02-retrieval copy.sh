83486f3ecdd31cea1d1815df6003abd1f4d9f462
Jannik Kuhlmann <mail@jannikkuhlmann.de>
2026-06-11 17:09:11 +0200

    Refractor sh

---
+++ sh/A-02-retrieval copy.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --gres=gpu:1
+
+#SBATCH --mem=50G
+#SBATCH --cpus-per-task=8
+#SBATCH --partition=gpuh200mini
+
+#SBATCH --time=00:05:00
+
+#SBATCH --output=/scratch/tmp/jkuhlma1/logs/%j_out.log    # stdout → Datei (%j = Job-ID)
+#SBATCH --error=/scratch/tmp/jkuhlma1/logs/%j_err.log     # stderr → Datei
+
+#SBATCH --job-name=Refractor-A-02-retrieval-test
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=jannik.kuhlmann@uni-muenster.de 
+
+# LOAD MODULES
+ml palma/2024a GCCcore/13.3.0 Python/3.12.3 CUDA/13.0.2
+
+# SOURCE PYTHON VENV
+source $HOME/venvs/colembed3Bv2-h200mini/bin/activate
+
+# SET EXISTING CHACHE
+export HF_HOME=$WORK/cache/huggingface
+export CUDA_HOME=$EBROOTCUDA
+export PIP_CACHE_DIR=$WORK/.cache/pip
+export RUN_TS=$(date +%m%d_%H%M)
+
+# pip install --upgrade pip
+# pip install wheel torch transformers pymupdf Pillow python-dotenv datasets polars pydantic psutil accelerate torchvision pypdf cryptography pandas
+
+## https://github.com/mjun0812/flash-attention-prebuild-wheels => Because bulidng them from scratch takes very very long time
+# pip install https://github.com/mjun0812/flash-attention-prebuild-wheels/releases/download/v0.9.17/flash_attn-2.8.3+cu130torch2.12-cp312-cp312-linux_x86_64.whl
+## Laut Nvidia die benutzte Verison
+# pip install https://github.com/mjun0812/flash-attention-prebuild-wheels/releases/download/v0.9.17/flash_attn-2.6.3+cu130torch2.12-cp312-cp312-linux_x86_64.whl
+
+## ALTERNATIV selbst bauen
+# pip install flash-attn==2.6.3 --no-build-isolation
+
+# START THE APPLICATION
+python -u $HOME/2026_BA_Code/src/pipelines/pipelineA/Refractor-A-02-retrieval.py
+
+# python -u $HOME/2026_BA_Code/evaluations/A-02/A-02.py
+
+# pip freeze > ~/2026_BA_Code/requirements_$(date +%m%d_%H%M).txt
