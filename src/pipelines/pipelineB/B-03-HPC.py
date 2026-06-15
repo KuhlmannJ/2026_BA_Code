da485cc3964b1fac076741fd4aa4beb698e09657
Jannik Kuhlmann <mail@jannikkuhlmann.de>
2026-06-15 17:41:18 +0200

    Typo

--- src/pipelines/pipelineB/B-03-HPC.py
+++ src/pipelines/pipelineB/B-03-HPC.py
@@ -188,7 +188,7 @@ for pdf_path in sorted(RETRIEVAL_LIST):
     # An "<|im_end|>" is always at the end of the output, needs to be removed
     # .strip() drops random empty lines
     output_clean = strip_thinking(output_text).replace("<|im_end|>", "").strip()
-    print(output_clean)
+
     output_JSON = json.loads(output_clean)
 
     # Saving that outout as JSON
