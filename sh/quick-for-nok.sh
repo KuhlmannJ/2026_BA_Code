d1811e66530d2f56ebccbff8f73b01f404b1554c
Jannik Kuhlmann <mail@jannikkuhlmann.de>
2026-06-20 15:37:07 +0200

    Quick4Nok

---
+++ sh/quick-for-nok.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --gres=gpu:1
+
+#SBATCH --mem=100G
+#SBATCH --cpus-per-task=16
+#SBATCH --partition=gpuh200  # Is enough as VRAM peak = 29GB for 1,5 min with 22GB RAM
+
+#SBATCH --time=00:30:00
+
+#SBATCH --output=/scratch/tmp/jkuhlma1/logs/%j_out.log    # stdout → Datei (%j = Job-ID)
+#SBATCH --error=/scratch/tmp/jkuhlma1/logs/%j_out.log     # stderr → Datei
+
+#SBATCH --job-name=quick4nok
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=jannik.kuhlmann@uni-muenster.de 
+
+# LOAD MODULES
+ml palma/2024a GCCcore/13.3.0 Python/3.12.3 CUDA/13.0.2
+
+# SOURCE PYTHON VENV
+source $HOME/venvs/colembed3Bv2-h200mini/bin/activate
+
+# SET EXISTING CHACHE
+export HF_HOME=$WORK/cache/huggingface
+export CUDA_HOME=$EBROOTCUDA
+export PIP_CACHE_DIR=$WORK/.cache/pip
+export RUN_TS=$(date +%m%d_%H%M) # To map the only the new retirevals
+
+# START THE APPLICATION
+# Set a flag '-t' for Testing (just one report)
+# python -u "$HOME/2026_BA_Code/src/pipelines/pipelineB/B-03-HPC.py" "$@"
+
+python -u "$HOME/2026_BA_Code/src/pipelines/pipelineB/B-03-HPC.py" -m moe -t
+python -u "$HOME/2026_BA_Code/src/pipelines/pipelineB/B-03-HPC.py" -m instr -t
+python -u "$HOME/2026_BA_Code/src/pipelines/pipelineB/B-03-HPC.py" -m think -t
+python -u "$HOME/2026_BA_Code/src/pipelines/pipelineB/B-03-HPC.py" -m moe -t
+python -u "$HOME/2026_BA_Code/src/pipelines/pipelineB/B-03-HPC.py" -m instr -t
